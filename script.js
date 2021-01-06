5a1e0fec4e828791ec386e9d9b8a8abf02a2464f
Jordan Masone <jordanmasone1@gmail.com>
2021-01-05 22:27:09 -0500

    Added JSON.parse to the getItem.

--- script.js
+++ script.js
@@ -30,22 +30,23 @@ $(document).ready(function () {
         //grab the user inputs that they put in
         var userTask = $(this).siblings(".description").val();
         var timeBlock = $(this).parent().attr("id");
-    
+
+        //console log the info
         console.log("task = ", userTask);
         console.log("time-slot =", timeBlock);
-    
-        // save the user inputs and where they put them in local storage
+
+        // save the user inputs and change them to strings
         localStorage.setItem(timeBlock, JSON.stringify(userTask))
       });
 
       //access the user's tasks after refresh
-      $('#hour-9 .description').val(localStorage.getItem("hour-9"));
-      $('#hour-10 .description').val(localStorage.getItem("hour-10"));
-      $('#hour-11 .description').val(localStorage.getItem("hour-11"));
-      $('#hour-12 .description').val(localStorage.getItem("hour-12"));
-      $('#hour-13 .description').val(localStorage.getItem("hour-13"));
-      $('#hour-14 .description').val(localStorage.getItem("hour-14"));
-      $('#hour-15 .description').val(localStorage.getItem("hour-15"));
-      $('#hour-16 .description').val(localStorage.getItem("hour-16"));
-      $('#hour-17 .description').val(localStorage.getItem("hour-17"));
+      $('#hour-9 .description').val(JSON.parse(localStorage.getItem("hour-9")));
+      $('#hour-10 .description').val(JSON.parse(localStorage.getItem("hour-10")));
+      $('#hour-11 .description').val(JSON.parse(localStorage.getItem("hour-11")));
+      $('#hour-12 .description').val(JSON.parse(localStorage.getItem("hour-12")));
+      $('#hour-13 .description').val(JSON.parse(localStorage.getItem("hour-13")));
+      $('#hour-14 .description').val(JSON.parse(localStorage.getItem("hour-14")));
+      $('#hour-15 .description').val(JSON.parse(localStorage.getItem("hour-15")));
+      $('#hour-16 .description').val(JSON.parse(localStorage.getItem("hour-16")));
+      $('#hour-17 .description').val(JSON.parse(localStorage.getItem("hour-17")));
 });
