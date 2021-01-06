e6e3e088183402d3176d7f8e780028c0f8d77651
Jordan Masone <jordanmasone1@gmail.com>
2021-01-05 22:23:27 -0500

    added getItem to save input after refresh.

--- script.js
+++ script.js
@@ -27,14 +27,25 @@ $(document).ready(function () {
     })
 
     $(".saveBtn").on("click", function() {
-        // get nearby values
+        //grab the user inputs that they put in
         var userTask = $(this).siblings(".description").val();
         var timeBlock = $(this).parent().attr("id");
     
         console.log("task = ", userTask);
         console.log("time-slot =", timeBlock);
     
-        // save the value in localStorage as time
+        // save the user inputs and where they put them in local storage
         localStorage.setItem(timeBlock, JSON.stringify(userTask))
       });
-})
+
+      //access the user's tasks after refresh
+      $('#hour-9 .description').val(localStorage.getItem("hour-9"));
+      $('#hour-10 .description').val(localStorage.getItem("hour-10"));
+      $('#hour-11 .description').val(localStorage.getItem("hour-11"));
+      $('#hour-12 .description').val(localStorage.getItem("hour-12"));
+      $('#hour-13 .description').val(localStorage.getItem("hour-13"));
+      $('#hour-14 .description').val(localStorage.getItem("hour-14"));
+      $('#hour-15 .description').val(localStorage.getItem("hour-15"));
+      $('#hour-16 .description').val(localStorage.getItem("hour-16"));
+      $('#hour-17 .description').val(localStorage.getItem("hour-17"));
+});
